78a3ee689b78129824aee1646c06e783e1beb917
Karim Motaz <karemmotaz2929@gmail.com>
2026-01-19 21:41:44 +0200

    Make Meditations View UI part 2

--- lib/features/meditations/presentation/widgets/categories_section/categories_section.dart
+++ lib/features/meditations/presentation/widgets/categories_section/categories_section.dart
@@ -1,21 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:moodly/core/constants/constants.dart';
 import 'package:moodly/features/meditations/data/models/category_enum.dart';
 import 'package:moodly/features/meditations/presentation/widgets/categories_section/active_category_button.dart';
+import 'package:moodly/features/meditations/presentation/widgets/categories_section/un_active_category_button.dart';
 
-class CategoriesSection extends StatelessWidget {
+class CategoriesSection extends StatefulWidget {
   const CategoriesSection({super.key});
+
+  @override
+  State<CategoriesSection> createState() => _CategoriesSectionState();
+}
+
+class _CategoriesSectionState extends State<CategoriesSection> {
+  Category selectedCategory = Category.values.first;
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: 31.36,
+      height: 33.36,
       child: ListView.separated(
         scrollDirection: Axis.horizontal,
-        padding: const EdgeInsets.symmetric(horizontal: 16),
+        padding: const EdgeInsets.symmetric(horizontal: kAppHorizontalPadding),
         itemCount: Category.values.length,
-        separatorBuilder: (_, __) => const SizedBox(width: 12),
+        separatorBuilder: (_, __) => const SizedBox(width: 5.34),
         itemBuilder: (context, index) {
-          return ActiveCategoryButton(category: Category.values[index]);
+          final category = Category.values[index];
+          final isActive = selectedCategory == category;
+
+          return isActive
+              ? ActiveCategoryButton(
+                  category: category,
+                  onTap: () {
+                    setState(() {
+                      selectedCategory = category;
+                    });
+                  },
+                )
+              : UnActiveCategoryButton(
+                  category: category,
+                  onTap: () {
+                    setState(() {
+                      selectedCategory = category;
+                    });
+                  },
+                );
         },
       ),
     );
--- lib/features/meditations/presentation/widgets/categories_section/un_active_category_button.dart
+++ lib/features/meditations/presentation/widgets/categories_section/un_active_category_button.dart
@@ -5,22 +5,29 @@ import 'package:moodly/core/theming/app_styles.dart';
 import 'package:moodly/features/meditations/data/models/category_enum.dart';
 
 class UnActiveCategoryButton extends StatelessWidget {
-  const UnActiveCategoryButton({super.key, required this.category});
-
+  const UnActiveCategoryButton({
+    super.key,
+    required this.category,
+    required this.onTap,
+  });
+  final VoidCallback onTap;
   final Category category;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 16),
-      decoration: BoxDecoration(
-        color: AppColors.borderButton.withAlpha(alphaFromPercentage(6)),
-        borderRadius: BorderRadius.circular(333),
-      ),
-      child: Center(
-        child: Text(
-          category.title,
-          style: AppStyles.extraBold14.copyWith(color: AppColors.bodyGray),
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        decoration: BoxDecoration(
+          color: AppColors.borderButton.withAlpha(alphaFromPercentage(6)),
+          borderRadius: BorderRadius.circular(333),
+        ),
+        child: Center(
+          child: Text(
+            category.title,
+            style: AppStyles.extraBold14.copyWith(color: AppColors.bodyGray),
+          ),
         ),
       ),
     );
