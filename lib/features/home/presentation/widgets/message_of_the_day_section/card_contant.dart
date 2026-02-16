af1a71cc286c4e612925b5ea8296032f5aeb3cbd
Karim Motaz <karemmotaz2929@gmail.com>
2026-02-16 19:59:25 +0200

    Make Main Card Responsive

--- lib/features/home/presentation/widgets/message_of_the_day_section/card_contant.dart
+++ lib/features/home/presentation/widgets/message_of_the_day_section/card_contant.dart
@@ -5,28 +5,54 @@ import '../../../../../core/theming/app_colors.dart';
 import '../../../../../core/theming/app_styles.dart';
 import '../../../data/models/quote_model.dart';
 
-class CardContant extends StatelessWidget {
+class CardContant extends StatefulWidget {
   const CardContant({super.key, required this.dailyQuote});
   final Quote dailyQuote;
 
   @override
+  State<CardContant> createState() => _CardContantState();
+}
+
+class _CardContantState extends State<CardContant> {
+  bool isExpanded = false;
+
+  @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        Text(
-          '"${dailyQuote.quote}"',
-          style: AppStyles.extraBold20WithShadow.copyWith(color: Colors.white),
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          isExpanded = !isExpanded;
+        });
+      },
+      child: AnimatedSize(
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.easeInOut,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              '"${widget.dailyQuote.quote}"',
+              maxLines: isExpanded ? null : 3,
+              overflow: isExpanded
+                  ? TextOverflow.visible
+                  : TextOverflow.ellipsis,
+              style: AppStyles.extraBold20WithShadow.copyWith(
+                color: Colors.white,
+              ),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              "— ${widget.dailyQuote.author}",
+              style: AppStyles.extraBold16.copyWith(
+                color: AppColors.borderButton.withAlpha(
+                  alphaFromPercentage(70),
+                ),
+              ),
+            ),
+          ],
         ),
-        const SizedBox(height: 10),
-        Text(
-          "— ${dailyQuote.author}",
-          style: AppStyles.extraBold16.copyWith(
-            color: AppColors.borderButton.withAlpha(alphaFromPercentage(70)),
-          ),
-        ),
-      ],
+      ),
     );
   }
 }
