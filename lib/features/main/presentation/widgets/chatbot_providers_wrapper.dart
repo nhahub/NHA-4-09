225f2e58fb033f8b300a0ac317ec949719e8ac13
Karim Motaz <karemmotaz2929@gmail.com>
2026-05-18 08:39:35 +0300

    feat(network): add network status listener to retry requests on reconnection

--- lib/features/main/presentation/widgets/chatbot_providers_wrapper.dart
+++ lib/features/main/presentation/widgets/chatbot_providers_wrapper.dart
@@ -1,20 +1,6 @@
-/*import 'package:flutter/material.dart';
-
-import '../../../chatbot/presentation/views/start_chat_view.dart';
-
-class ChatbotProvidersWrapper extends StatelessWidget {
-  final bool isPremium;
-
-  const ChatbotProvidersWrapper({super.key, required this.isPremium});
-
-  @override
-  Widget build(BuildContext context) {
-    return StartChatView(isPremium: isPremium,);
-  }
-}*/
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
+import '../../../../core/manager/network_cubit/network_cubit.dart';
 import '../../../../core/services/get_it_service.dart';
 import '../../../chatbot/data/repos/chatbot_repo.dart';
 import '../../../chatbot/presentation/manager/chatbot_cubit/chatbot_cubit.dart';
@@ -24,13 +10,23 @@ class ChatbotProvidersWrapper extends StatelessWidget {
   final bool isPremium;
 
   const ChatbotProvidersWrapper({super.key, required this.isPremium});
+  void _retryRequests(BuildContext context) {
+    context.read<ChatbotCubit>().loadSessions();
+  }
 
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (context) =>
           ChatbotCubit(repo: getIt.get<ChatbotRepo>())..loadSessions(),
-      child: ChatSessionsView(isPremium: isPremium),
+      child: BlocListener<NetworkCubit, NetworkState>(
+        listener: (context, state) {
+          if (state.status == NetworkStatus.reconnected) {
+            _retryRequests(context);
+          }
+        },
+        child: ChatSessionsView(isPremium: isPremium),
+      ),
     );
   }
 }
--- lib/features/main/presentation/widgets/community_providers_wrapper.dart
+++ lib/features/main/presentation/widgets/community_providers_wrapper.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../../../core/manager/network_cubit/network_cubit.dart';
 import '../../../../core/services/get_it_service.dart';
 import '../../../community/data/repos/create_post_repo.dart';
 import '../../../community/presentation/manager/community_feed_cubit/community_feed_cubit.dart';
@@ -9,6 +10,9 @@ import '../../../community/presentation/views/community_view.dart';
 class CommunityProvidersWrapper extends StatelessWidget {
   final bool isPremium;
   const CommunityProvidersWrapper({super.key, required this.isPremium});
+  void _retryRequests(BuildContext context) {
+    context.read<CommunityFeedCubit>().getPosts();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,7 +20,14 @@ class CommunityProvidersWrapper extends StatelessWidget {
       create: (context) =>
           CommunityFeedCubit(createPostRepo: getIt.get<CreatePostRepo>())
             ..getPosts(),
-      child: CommunityView(isPremium: isPremium),
+      child: BlocListener<NetworkCubit, NetworkState>(
+        listener: (context, state) {
+          if (state.status == NetworkStatus.reconnected) {
+            _retryRequests(context);
+          }
+        },
+        child: CommunityView(isPremium: isPremium),
+      ),
     );
   }
 }
